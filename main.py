401f164a7d5b5bbcb35485167e922ced660c5874
Benkivuva <benkivuvak5@gmail.com>
2023-09-27 18:53:47 +0300

    update

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self, name, test):
+            return {"name": name, "test": test}
+
+api.add_resource(HelloWorld, "/hellombole/<string:name>/int:test")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
