dd0b96ef873c28ab17c885ba635e5a3361f4b2b3
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 12:32:36 +0300

    status codes

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "video/1", {"likes": 10})
+response = requests.post(BASE + "video/1", {"likes": 10, "name": "dembele", "views": 219})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
