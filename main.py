6afd8a18e7d0f6dbe7e3221c7b4ecd2d9bf577d0
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 15:54:16 +0300

    adding entries to db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,33 +17,32 @@ class VideoModel(db.Model):
     def __repr__(self):
         return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
 
-db.create_all()
-
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-videos = {}
-
-def abort_if_dont_exist(video_id):
-    if video_id not in videos:
-        abort(404, message ="video id isn't valid...")
-
-def abort_if_exist(video_id):
-    if video_id in videos:
-        abort(409, message ="video already exists with that ID...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_dont_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
+        return result
 
+    @marshal_with(resource_fields)
     def post(self, video_id):
-        abort_if_exist(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
 
     def delete(self, video_id):
         abort_if_dont_exist(video_id)
