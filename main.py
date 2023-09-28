319c700514b08cce8ebf32f1e7dde8680605d82d
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 16:38:14 +0300

    improve documentation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
-from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+"""
+This script defines a Flask RESTful API to manage video data.
+"""
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,6 +12,7 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+# Define the VideoModel class to represent video data
 class VideoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -15,18 +20,24 @@ class VideoModel(db.Model):
     likes = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Video(name = {self.name}, views = {self.views}, likes = {self.likes})"
+        return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
 
+# Create the database tables
+db.create_all()
+
+# Request parser for video creation
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
+# Request parser for video update
 video_update_args = reqparse.RequestParser()
 video_update_args.add_argument("name", type=str, help="Name of the video is required")
 video_update_args.add_argument("views", type=int, help="Views of the video")
 video_update_args.add_argument("likes", type=int, help="Likes on the video")
 
+# Define the resource fields for marshalling responses
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -35,50 +46,69 @@ resource_fields = {
 }
 
 class Video(Resource):
-	@marshal_with(resource_fields)
-	def get(self, video_id):
-		result = VideoModel.query.filter_by(id=video_id).first()
-		if not result:
-			abort(404, message="Could not find video with that id")
-		return result
+    """
+    Video resource class to handle API requests related to videos.
+    """
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        """
+        Get video by ID.
+        """
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
+        return result
 
-	@marshal_with(resource_fields)
-	def post(self, video_id):
-		args = video_put_args.parse_args()
-		result = VideoModel.query.filter_by(id=video_id).first()
-		if result:
-			abort(409, message="Video id taken...")
+    @marshal_with(resource_fields)
+    def post(self, video_id):
+        """
+        Create a new video.
+        """
+        args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
 
-		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-		db.session.add(video)
-		db.session.commit()
-		return video, 201
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
-	@marshal_with(resource_fields)
-	def patch(self, video_id):
-		args = video_update_args.parse_args()
-		result = VideoModel.query.filter_by(id=video_id).first()
-		if not result:
-			abort(404, message="Video doesn't exist, cannot update")
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        """
+        Update an existing video.
+        """
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
 
-		if args['name']:
-			result.name = args['name']
-		if args['views']:
-			result.views = args['views']
-		if args['likes']:
-			result.likes = args['likes']
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
 
-		db.session.commit()
+        db.session.commit()
+        return result
 
-		return result
+    def delete(self, video_id):
+        """
+        Delete a video by its ID.
+        """
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if not video:
+            abort(404, message="Video doesn't exist, cannot delete")
 
+        db.session.delete(video)
+        db.session.commit()
+        return '', 204
 
-	def delete(self, video_id):
-		abort_if_video_id_doesnt_exist(video_id)
-		del videos[video_id]
-		return '', 204
-
+# Add the Video resource to the API with the specified endpoint
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
+    # Run the Flask app
     app.run(host="127.0.0.1", port=5000, debug=True)
--- test.py
+++ test.py
@@ -7,12 +7,46 @@ data = [
     {"likes": 8, "name": "funny_cats", "views": 55000},
     {"likes": 15, "name": "epic_adventures", "views": 123456},
     {"likes": 10, "name": "cooking_masterclass", "views": 9800},
-    {"likes": 20, "name": "travel_diaries", "views": 75000}]
+    {"likes": 20, "name": "travel_diaries", "views": 75000}
+]
 
+# Function to send a DELETE request for a video
+def delete_video(video_id):
+    response = requests.delete(BASE + "video/" + str(video_id))
+    print("DELETE Response for video", video_id, ":", response.text)
+
+# Function to send a PATCH request to update a video
+def update_video(video_id, updated_data):
+    response = requests.patch(BASE + "video/" + str(video_id), json=updated_data)
+    print("PATCH Response for video", video_id, ":", response.json())
+
+# Iterate through the data and send POST requests to create videos
+for i, video in enumerate(data):
+    response = requests.post(BASE + "video/" + str(i), json=video)
+    print("POST Response for video", i, ":", response.json())
+
+# Wait for user input
+input("Press Enter to continue...")
+
+# Send GET requests to retrieve the created videos
 for i in range(len(data)):
-    response = requests.post(BASE + "video/" + str(i), data[i])
-    print(response.json())
+    response = requests.get(BASE + "video/" + str(i))
+    print("GET Response for video", i, ":", response.json())
 
-input()
-response = requests.get(BASE + "video/2")
-print(response.json())
+# Update the first video
+updated_data = {"likes": 30, "views": 100000}
+update_video(0, updated_data)
+
+# Wait for user input
+input("Press Enter to continue...")
+
+# Delete the second video
+delete_video(1)
+
+# Wait for user input
+input("Press Enter to continue...")
+
+# Try to get the deleted video (should return a 404 status code)
+response = requests.get(BASE + "video/1")
+print("GET Response for deleted video:", response.status_code)
+
