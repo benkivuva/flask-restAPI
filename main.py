1acfe583fcfe53fb2386794f3eadaf10fffa18fe
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 11:29:31 +0300

    fix error

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class HelloWorld(Resource):
     def get(self, name, test):
             return {"name": name, "test": test}
 
-api.add_resource(HelloWorld, "/hellombole/<string:name>/int:test")
+api.add_resource(HelloWorld, "/hellombole/<string:name>/<int:test>")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
