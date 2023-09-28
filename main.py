f1f1de90864525e62262c3918bb872f5efc3ea5b
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 12:21:45 +0300

    request arguement parser

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"ben": {"age": 34, "gender": "male"}, "mbole": {"age": 43, "gender": "male"}}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
-class HelloWorld(Resource):
-    def get(self, name):
-            return names[name]
+videos = {}
 
-api.add_resource(HelloWorld, "/hellombole/<string:name>")
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def post(self, video_id):
+        args = video_put_args.parse_args()
+        
+        return {video_id: args}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
