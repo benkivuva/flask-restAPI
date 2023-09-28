8259d41829e20c7543674f6490f91f3cd250ff9f
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 15:01:01 +0300

    handle delete requests

--- main.py
+++ main.py
@@ -12,19 +12,29 @@ video_put_args.add_argument("likes", type=int, help="Likes on the video", requir
 
 videos = {}
 
-def abort_if(video_id):
+def abort_if_dont_exist(video_id):
     if video_id not in videos:
         abort(404, message ="video id isn't valid...")
 
+def abort_if_exist(video_id):
+    if video_id in videos:
+        abort(409, message ="video already exists with that ID...")
+
 class Video(Resource):
     def get(self, video_id):
-        abort_if(video_id)
+        abort_if_dont_exist(video_id)
         return videos[video_id]
 
     def post(self, video_id):
+        abort_if_exist(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_dont_exist(video_id)
+        del videos[video_id]
+        return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,8 +2,21 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-#response = requests.post(BASE + "video/1", {"likes": 10, "name": "dembele", "views": 219})
-#print(response.json())
-#input()
-response = requests.get(BASE + "video/6")
+data = [
+    {"likes": 12, "name": "mwawasi", "views": 21900},
+    {"likes": 8, "name": "funny_cats", "views": 55000},
+    {"likes": 15, "name": "epic_adventures", "views": 123456},
+    {"likes": 10, "name": "cooking_masterclass", "views": 9800},
+    {"likes": 20, "name": "travel_diaries", "views": 75000}]
+
+for i in range(len(data)):
+    response = requests.post(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
