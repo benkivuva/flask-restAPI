0190a394fc69ed87154708044e2527f93b6e4778
Benkivuva <benkivuvak5@gmail.com>
2023-09-28 16:17:08 +0300

    handle updates

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
+
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -30,24 +35,48 @@ resource_fields = {
 }
 
 class Video(Resource):
-    @marshal_with(resource_fields)
-    def get(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if not result:
-            abort(404, message="Could not find video with that id")
-        return result
+	@marshal_with(resource_fields)
+	def get(self, video_id):
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		return result
 
-    @marshal_with(resource_fields)
-    def post(self, video_id):
-        args = video_put_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if result:
-            abort(409, message="Video id taken...")
+	@marshal_with(resource_fields)
+	def post(self, video_id):
+		args = video_put_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if result:
+			abort(409, message="Video id taken...")
 
-    def delete(self, video_id):
-        abort_if_dont_exist(video_id)
-        del videos[video_id]
-        return '', 204
+		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+		db.session.add(video)
+		db.session.commit()
+		return video, 201
+
+	@marshal_with(resource_fields)
+	def patch(self, video_id):
+		args = video_update_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Video doesn't exist, cannot update")
+
+		if args['name']:
+			result.name = args['name']
+		if args['views']:
+			result.views = args['views']
+		if args['likes']:
+			result.likes = args['likes']
+
+		db.session.commit()
+
+		return result
+
+
+	def delete(self, video_id):
+		abort_if_video_id_doesnt_exist(video_id)
+		del videos[video_id]
+		return '', 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
